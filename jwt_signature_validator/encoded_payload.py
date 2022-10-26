2ff07ac8d3150b820d4c44e0fd84f0512dc1c33a
noureen taj <noureen.taj@knowledgelens.com>
2022-10-26 20:19:19 +0530

    fix: raise error on content type validation exception instead of 500 crash

--- jwt_signature_validator/encoded_payload.py
+++ jwt_signature_validator/encoded_payload.py
@@ -32,11 +32,11 @@ ENFORCE_DOMAIN_WILDCARD = "Domain wildcard patterns must be like '*.example.com'
 
 class EncodedPayloadSignatureMiddleware:
     def __init__(
-        self,
-        app,
-        jwt_secret: str,
-        jwt_algorithms: list[str],
-        protect_hosts: list = None,
+            self,
+            app,
+            jwt_secret: str,
+            jwt_algorithms: list[str],
+            protect_hosts: list = None,
     ):
         self.app = app
         self.protect_hosts = protect_hosts
@@ -72,10 +72,10 @@ class EncodedPayloadSignatureMiddleware:
             try:
                 signature = jwt.decode(signature, self.jwt_secret, self.jwt_algorithms)
             except (
-                InvalidSignatureError,
-                ExpiredSignatureError,
-                MissingRequiredClaimError,
-                DecodeError,
+                    InvalidSignatureError,
+                    ExpiredSignatureError,
+                    MissingRequiredClaimError,
+                    DecodeError,
             ) as inv_exp:
                 logging.error(inv_exp)
                 raise HTTPException(
@@ -84,18 +84,23 @@ class EncodedPayloadSignatureMiddleware:
             signature = json.dumps(signature).encode()
             return {"type": receive_["type"], "body": signature, "more_body": False}
 
-        headers = MutableHeaders(scope=scope)
-        if (
-            headers.get("Content-Type") is None
-            and scope.get("method", "POST") in self.validate_request_types
-        ):
+        async def content_type_validation_failed():
             raise HTTPException(status_code=406, detail="Unacceptable Content Type!")
+
+        headers = MutableHeaders(scope=scope)
+
+        if (
+                headers.get("Content-Type") is None
+                and scope.get("method", "POST") in self.validate_request_types
+        ):
+            await self.app(scope, content_type_validation_failed, send)
+            return
         elif headers.get("Content-Type") == "application/json":
             host = headers.get("host", "").split(":")[0]
             is_protected_host = False
             for pattern in self.protect_hosts:
                 if host == pattern or (
-                    pattern.startswith("*") and host.endswith(pattern[1:])
+                        pattern.startswith("*") and host.endswith(pattern[1:])
                 ):
                     is_protected_host = True
                     break
